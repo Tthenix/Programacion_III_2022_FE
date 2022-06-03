b076cbcc36b0b9bc78219fc0c1fb4682dd8f4ebe
Tthenix <nahuelfacundox@gmail.com>
2022-06-03 12:00:50 -0300

    Home mejorado

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import Img1 from "../Imgenes/Img1.jpg"
+import Img1 from "../Imgenes/image.jpg"
 import "../Home/home.css"
+import { Button } from 'bootstrap';
 
 function Home() {
 
-  return (
-      <div align="center"> 
-        <div class="Fondo"> 
-          <img src={Img1} width="100%" height="100%" /> 
-        </div>
-        <div class="Bienvenidos">
-          <p>Bienvenidos</p>
-        </div>
-      
-        <h2>1°Año</h2>
-        <h2>2°Año</h2>
-        <h2>3°Año</h2>
-        <h2>4°Año</h2>
-        <h2>5°Año</h2>
-        <h2>6°Año</h2>
-      </div>
-  )
+    return ( <
+        div align = "center" >
+        <
+        div class = "fondo" >
+        <
+        img src = { Img1 }
+        width = "100%"
+        height = "100%" / >
+        <
+        p > Bienvenidos < /p> <
+        /div> <
+        div >
+
+        <
+        h2 > 2° Año < /h2> <
+        h2 > 3° Año < /h2> <
+        h2 > 4° Año < /h2> <
+        h2 > 5° Año < /h2> <
+        h2 > 6° Año < /h2> <
+        /div> <
+        /div>
+    )
 }
 
 export default Home
